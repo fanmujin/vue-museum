4dc4469ca11bf12e4bafd59018193515bed35e0a
muzi_pro <muzi_pro@163.com>
2019-05-05 20:42:26 +0800

    添加less，axios

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,17 @@ import Login from './views/Login.vue'
 import NotFound from './views/404.vue'
 import Home from './views/Home.vue'
 import Main from './views/Main.vue'
-import Table from './views/nav1/Table.vue'
+import Admin from './views/nav1/Admin.vue'
 import Form from './views/nav1/Form.vue'
-import user from './views/nav1/user.vue'
+import Display from './views/nav1/Display'
+import Culture from './views/nav1/Culture'
 import Page4 from './views/nav2/Page4.vue'
 import Page5 from './views/nav2/Page5.vue'
-import Page6 from './views/nav3/Page6.vue'
+import Page6 from './views/nav2/Page6.vue'
+import Page7 from './views/nav3/Page7.vue'
 import echarts from './views/charts/echarts.vue'
-
+import addressChart from './views/charts/address-chart.vue'
+import address from './views/charts/address.vue'
 let routes = [
     {
         path: '/login',
@@ -27,42 +30,46 @@ let routes = [
     {
         path: '/',
         component: Home,
-        name: '导航一',
-        iconCls: 'el-icon-message',//图标样式class
+        name: '一览表',
+        iconCls: 'fa fa-list',//图标样式class
         children: [
-            { path: '/main', component: Main, name: '主页', hidden: true },
-            { path: '/table', component: Table, name: 'Table' },
-            { path: '/form', component: Form, name: 'Form' },
-            { path: '/user', component: user, name: '列表' },
+            { path: '/main', component: Main, name: '主页'},
+            { path: '/admin', component: Admin, name: '用户列表'},
+            { path: '/form', component: Form, name: '会展安排' },
+            { path: '/display', component: Display, name: '展览列表' },
+            { path: '/culture', component: Culture, name: '名俗列表' }
+
         ]
     },
     {
         path: '/',
         component: Home,
-        name: '导航二',
-        iconCls: 'fa fa-id-card-o',
+        name: '数字信息',
+        iconCls: 'fa fa-bullhorn',
         children: [
-            { path: '/page4', component: Page4, name: '页面4' },
-            { path: '/page5', component: Page5, name: '页面5' }
+            { path: '/page4', component: Page4, name: '网上展厅' },
+            { path: '/page5', component: Page5, name: '消息通知' },
+            { path: '/page6', component: Page6, name: '视频管理' }
         ]
     },
     {
         path: '/',
         component: Home,
         name: '',
-        iconCls: 'fa fa-address-card',
+        iconCls: 'fa fa-file-text',
         leaf: true,//只有一个节点
         children: [
-            { path: '/page6', component: Page6, name: '导航三' }
+            { path: '/page7', component: Page7, name: '学术报告' }
         ]
     },
     {
         path: '/',
         component: Home,
-        name: 'Charts',
-        iconCls: 'fa fa-bar-chart',
+        name: '地理信息',
+        iconCls: 'fa fa-map-marker',
         children: [
-            { path: '/echarts', component: echarts, name: 'echarts' }
+            { path: '/address', component: address, name: '地理分布' },
+            { path: '/addressChart', component: addressChart, name: '地理统计' }
         ]
     },
     {
