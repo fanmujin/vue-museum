6bf38dd782576cce78bb5459466d1b5576016719
muzi_pro <muzi_pro@163.com>
2019-05-07 15:58:40 +0800

    完成展览的添加页面

--- src/views/nav1/Form.vue
+++ src/views/nav1/Form.vue
@@ -1,10 +1,10 @@
 <template>
-	<el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
+	<el-form ref="form" :model="form1" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
 		<el-form-item label="展览名称">
-			<el-input v-model="form.name"></el-input>
+			<el-input v-model="form1.name"></el-input>
 		</el-form-item>
 		<el-form-item label="展厅">
-			<el-select v-model="form.region" placeholder="请选择展览区">
+			<el-select v-model="form1.region" placeholder="请选择展览区">
 				<el-option label="展厅一" value="1"></el-option>
 				<el-option label="展厅二" value="2"></el-option>
 				<el-option label="展厅三" value="3"></el-option>
@@ -14,24 +14,19 @@
 		</el-form-item>
 		<el-form-item label="展览时间">
 			<el-col :span="11">
-				<el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+				<el-date-picker
+						v-model="form1.date1" type="datetime" placeholder="选择日期时间">
+				</el-date-picker>
 			</el-col>
-			<el-col class="line" :span="2">-</el-col>
+			<el-col class="line" :span="2">至</el-col>
 			<el-col :span="11">
-				<el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-			</el-col>
-		</el-form-item>
-		<el-form-item label="结束时间">
-			<el-col :span="11">
-				<el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-			</el-col>
-			<el-col class="line" :span="2">-</el-col>
-			<el-col :span="11">
-				<el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+				<el-date-picker
+						v-model="form1.date2" type="datetime" placeholder="选择日期时间">
+				</el-date-picker>
 			</el-col>
 		</el-form-item>
 		<el-form-item label="展览性质">
-			<el-checkbox-group v-model="form.type">
+			<el-checkbox-group v-model="form1.type">
 				<el-checkbox label="艺术品展览" name="type"></el-checkbox>
 				<el-checkbox label="音乐展览" name="type"></el-checkbox>
 				<el-checkbox label="主题展览" name="type"></el-checkbox>
@@ -39,26 +34,27 @@
 			</el-checkbox-group>
 		</el-form-item>
 		<el-form-item label="主办单位">
-			<el-radio-group v-model="form.resource">
+			<el-radio-group v-model="form1.resource">
 				<el-radio label="河南非遗文创联盟"></el-radio>
 				<el-radio label="河南非物质文化遗产网"></el-radio>
 			</el-radio-group>
 		</el-form-item>
 		<el-form-item label="会展内容">
-			<el-input type="textarea" v-model="form.desc"></el-input>
+			<el-input type="textarea" v-model="form1.desc"></el-input>
 		</el-form-item>
 		<el-form-item>
-			<el-button type="primary" @click="create">立即创建</el-button>
+			<el-button type="primary" @click="createDiaplay">立即创建</el-button>
 			<el-button @click="clear">取消</el-button>
 		</el-form-item>
 	</el-form>
 </template>
 
 <script>
+	import util from '../../common/js/util'
 	export default {
 		data() {
 			return {
-				form: {
+				form1: {
 					name: '',
 					region: '',
 					date1: '',
@@ -74,8 +70,43 @@
 			onSubmit() {
 				console.log('submit!');
 			},
-			create:function () {
-				alert("创建成功");
+			// handleReset2() {
+			// 	this.$refs.form.resetFields();
+			// },
+			createDiaplay:function () {
+			//	alert("创建成功");
+				this.$refs.form.validate((valid) => {
+					if (valid) {
+						this.$confirm('确认提交吗？', '提示', {}).then(() => {
+							this.addLoading = true;
+							//NProgress.start();
+							let para = Object.assign({}, this.form1);
+							para.date1 = (!para.date1 || para.date1 == '') ? '' : util.formatDate.format(new Date(para.date1), 'yyyy-MM-dd hh:mm:ss');
+							para.date2 = (!para.date1 || para.date2 == '') ? '' : util.formatDate.format(new Date(para.date2), 'yyyy-MM-dd hh:mm:ss');
+							console.log("para"+para.name);
+							this.$axios({
+								method:"post",
+								url:"/api/dispaly/addDisplay",
+								params:{
+									displayTitle:para.name,
+									start : para.date1,
+									end : para.date2,
+									displayContend : para.desc
+								}
+							}).then((res) => {
+								this.addLoading = false;
+								//NProgress.done();
+								this.$message({
+									message: '提交成功',
+									type: 'success'
+								});
+								this.$refs['form'].resetFields();
+								//this.addFormVisible = false;
+								//this.getUsers();
+							});
+						});
+					}
+				});
 				this.form = {
 					name: '',
 					region: '',
@@ -88,7 +119,7 @@
 				}
 			},
 			clear:function () {
-				this.form = {
+				this.form1 = {
 					name: '',
 					region: '',
 					date1: '',
