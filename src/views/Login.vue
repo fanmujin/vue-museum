632519618ee51ee6096a837ed76f59226dabe3bc
muzi_pro <muzi_pro@163.com>
2019-05-09 10:24:10 +0800

    添加了注册功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,5 @@
 <template>
+  <section>
   <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
     <h3 class="title">系统登录</h3>
     <el-form-item prop="account">
@@ -10,14 +11,50 @@
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
       <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+      <el-button type="" style="width:100%;margin-top: 10px;margin-left: 0px" @click="handleAdd">立即注册</el-button>
       <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
     </el-form-item>
   </el-form>
+  <!--新增界面-->
+  <el-dialog title="Register" v-model="addFormVisible" :close-on-click-modal="false">
+    <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+      <el-form-item label="姓名" prop="name">
+        <el-input v-model="addForm.name" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="addForm.password" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="addForm.Email" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="联系方式" prop="Tel">
+        <el-input v-model="addForm.Tel" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-radio-group v-model="addForm.sex">
+          <el-radio class="radio" :label="1">男</el-radio>
+          <el-radio class="radio" :label="0">女</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="年龄">
+        <el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
+      </el-form-item>
+      <el-form-item label="生日">
+        <el-date-picker type="date" placeholder="选择日期" v-model="addForm.birth"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="地址">
+        <el-input type="textarea" v-model="addForm.addr"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click.native="addFormVisible = false">取消</el-button>
+      <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+    </div>
+  </el-dialog>
+  </section>
 </template>
 
 <script>
-  import { requestLogin } from '../api/api';
-  //import NProgress from 'nprogress'
   export default {
     data() {
       return {
@@ -28,15 +65,39 @@
         },
         rules2: {
           account: [
-            { required: true, message: '请输入账号', trigger: 'blur' },
+            {required: true, message: '请输入账号', trigger: 'blur'},
             //{ validator: validaePass }
           ],
           checkPass: [
-            { required: true, message: '请输入密码', trigger: 'blur' },
+            {required: true, message: '请输入密码', trigger: 'blur'},
             //{ validator: validaePass2 }
           ]
         },
-        checked: true
+        checked: true,
+        addFormVisible: false,//新增界面是否显示
+        addLoading: false,
+        addFormRules: {
+          name: [
+            { required: true, message: '请输入姓名', trigger: 'blur' }
+          ],
+          password:[
+            {required:true,message:'请输入密码',trigger:'blur'}
+          ],
+          Tel:[
+            {required:true,message:'请输入联系方式',trigger:'blur'}
+          ]
+        },
+        //新增界面数据
+        addForm: {
+          name: '',
+          password:'',
+          Email:'',
+          Tel:'',
+          sex: -1,
+          age: 0,
+          birth: '',
+          addr: ''
+        }
       };
     },
     methods: {
@@ -50,23 +111,19 @@
             //_this.$router.replace('/table');
             this.logining = true;
             //NProgress.start();
-            var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
-           // requestLogin(loginParams).then(data => {
+            var loginParams = {username: this.ruleForm2.account, password: this.ruleForm2.checkPass};
+            // requestLogin(loginParams).then(data => {
             this.$axios({
               method: "post",
-              url:"/api/Manager/doLogin",
-              params:{
-                   name:loginParams.username,
-                   password:loginParams.password
+              url: "/api/Manager/doLogin",
+              params: {
+                name: loginParams.username,
+                password: loginParams.password
               }
-              // data:{
-              //   name: loginParams.username,
-              //   password: loginParams.password
-              // }
-            }).then(res =>{
+            }).then(res => {
               this.logining = false;
               //NProgress.done();
-              let { msg, code, data } = res.data;
+              let {msg, code, data} = res.data;
               if (code != "1") {
                 this.$message({
                   message: msg,
@@ -75,7 +132,7 @@
               } else {
                 console.log(data.name);
                 sessionStorage.setItem('user', JSON.stringify(data.name));
-                this.$router.push({ path: '/main' });
+                this.$router.push({path: '/main'});
               }
             });
           } else {
@@ -83,10 +140,57 @@
             return false;
           }
         });
+      },
+      //显示新增界面
+      handleAdd: function () {
+        this.addFormVisible = true;
+        this.addForm = {
+          name: '',
+          password:'',
+          Email:'',
+          Tel:'',
+          sex: -1,
+          age: 0,
+          birth: '',
+          addr: ''
+        };
+      },
+      //新增
+      addSubmit: function () {
+        this.$refs.addForm.validate((valid) => {
+          if (valid) {
+            this.$confirm('确认提交吗？', '提示', {}).then(() => {
+              this.addLoading = true;
+              //NProgress.start();
+              let RegisterPara ={adminName:this.addForm.name,adminPassword:this.addForm.password,
+                adminEmail:this.addForm.Email,adminTel:this.addForm.Tel}
+             // para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
+              this.$axios({
+                method: "post",
+                url: "/api/Manager/Register",
+                params: {
+                  adminName:RegisterPara.adminName,
+                  adminPassword:RegisterPara.adminPassword,
+                  adminEmail:RegisterPara.adminEmail,
+                  adminTel:RegisterPara.adminTel
+                }
+              }).then((res) => {
+                this.addLoading = false;
+                //NProgress.done();
+                this.$message({
+                  message: '注册成功',
+                  type: 'success'
+                });
+                this.$refs['addForm'].resetFields();
+                this.addFormVisible = false;
+               //this.getUsers();
+              });
+            });
+          }
+        });
       }
     }
   }
-
 </script>
 
 <style lang="scss" scoped>
